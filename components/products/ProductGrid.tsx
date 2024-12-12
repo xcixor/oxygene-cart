9fc1d002c53be9fbd14715eacef7a5dd9021c087
xcixor <pndungu54@gmail.com>
2024-12-12 22:33:53 +0300

    Paginate index page

--- components/products/ProductGrid.tsx
+++ components/products/ProductGrid.tsx
@@ -1,15 +1,26 @@
-// components/products/ProductGrid.tsx
+"use client";
 
 import { Product } from "@/lib/api/faker-shop";
 import { ProductCard } from "@/components/products/ProductCard";
 import { Loader } from "@/components/ui/Loader";
+import PaginationControls from "@/components/PaginationControls";
+import { useSearchParams } from "next/navigation";
 
 interface ProductGridProps {
   products: Product[];
   isLoading?: boolean;
+  basePageUrl: string;
 }
 
-export function ProductGrid({ products, isLoading = false }: ProductGridProps) {
+export function ProductGrid({
+  products,
+  isLoading = false,
+  basePageUrl,
+}: ProductGridProps) {
+  const searchParams = useSearchParams();
+  const page = Number(searchParams.get("page")) || 1;
+  const ITEMS_PER_PAGE = 4;
+
   if (isLoading) {
     return <Loader />;
   }
@@ -22,11 +33,29 @@ export function ProductGrid({ products, isLoading = false }: ProductGridProps) {
     );
   }
 
+  // Calculate pagination
+  const start = (page - 1) * ITEMS_PER_PAGE;
+  const end = start + ITEMS_PER_PAGE;
+  const currentItems = products.slice(start, end);
+  const totalPages = Math.ceil(products.length / ITEMS_PER_PAGE);
+
   return (
-    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
+    <div className="space-y-6">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {currentItems.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+
+      {products.length > ITEMS_PER_PAGE && (
+        <PaginationControls
+          hasNextPage={totalPages > page}
+          hasPrevPage={page > 1}
+          totalPages={totalPages}
+          baseUrl={basePageUrl}
+          pageSize={ITEMS_PER_PAGE.toString()}
+        />
+      )}
     </div>
   );
 }
